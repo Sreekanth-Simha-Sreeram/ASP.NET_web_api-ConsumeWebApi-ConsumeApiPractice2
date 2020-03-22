56972629e3481fc474e2703d37517c567dd1284f
Jesuraj77 <isysraju2001@gmail.com>
2020-03-22 14:15:38 +0530

    code added

---
+++ ConsumeApiPractice2/Controllers/ConsumePostPutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConsumeApiPractice2.Controllers
+{
+    public class ConsumePostPutController : Controller
+    {
+      //code here  Perform how to initiate the POST method through forms.
+        // Show how to add new book resources and display the added book.
+
+        // code here to load resources on UI and update the item and post the data.
+
+    }
+}
